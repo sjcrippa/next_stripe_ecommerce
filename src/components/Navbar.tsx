fce910ffb12129fa4f0601dcc427524793fad5dc
Santiago Crippa <santicrippa@gmail.com>
2024-03-25 17:41:57 -0300

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { ShoppingBag } from "lucide-react";
 
 import { Button } from "./ui/button";
 
@@ -16,10 +17,10 @@ export default function Navbar() {
   const pathname = usePathname()
 
   return (
-    <header className="mb-8 border-b py-2">
+    <header className="mb-8 border-b">
       <nav className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
         <Link href='/'>
-          <h1 className="text-4xl font-bold">Next
+          <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold">Next
             <span className="text-primary">Commerce</span>
           </h1>
         </Link>
@@ -39,7 +40,12 @@ export default function Navbar() {
         </ul>
 
         <section className="flex divide-x border-r sm:border-l">
-          <Button className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none">Hey</Button>
+          <Button
+            variant={ "outline"}
+            className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none">
+            <ShoppingBag />
+            <span className="hidden text-xs font-semibold sm:block">Cart</span>
+          </Button>
         </section>
       </nav>
     </header>
